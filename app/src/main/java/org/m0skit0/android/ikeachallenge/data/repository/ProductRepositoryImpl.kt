4b0d1cbd013e9ea231efde58e6524fe69030e20f
m0skit0 <ametric@gmail.com>
2019-11-27 23:17:09 +0100

    Fixed return type for repository interface calls

--- app/src/main/java/org/m0skit0/android/ikeachallenge/data/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/org/m0skit0/android/ikeachallenge/data/repository/ProductRepositoryImpl.kt
@@ -4,18 +4,19 @@ import arrow.fx.IO
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import org.m0skit0.android.ikeachallenge.data.api.ProductApi
-import org.m0skit0.android.ikeachallenge.data.api.ProductsDto
+import org.m0skit0.android.ikeachallenge.domain.Product
 import org.m0skit0.android.ikeachallenge.domain.ProductRepository
+import org.m0skit0.android.ikeachallenge.domain.toProductsByType
 import org.m0skit0.android.ikeachallenge.error.APIException
 
 internal class ProductRepositoryImpl : ProductRepository, KoinComponent {
 
     private val productApi: ProductApi by inject()
 
-    override fun getProducts(): IO<ProductsDto> = IO {
+    override fun getProducts(): IO<List<Product>> = IO {
         productApi.getProducts().execute().run {
             if (isSuccessful) {
-                body() ?: throw APIException("Body is null")
+                body()?.toProductsByType() ?: throw APIException("Body is null")
             } else {
                 throw APIException(errorBody()?.string() ?: "Error body is null")
             }
--- app/src/main/java/org/m0skit0/android/ikeachallenge/domain/ProductRepository.kt
+++ app/src/main/java/org/m0skit0/android/ikeachallenge/domain/ProductRepository.kt
@@ -1,8 +1,7 @@
 package org.m0skit0.android.ikeachallenge.domain
 
 import arrow.fx.IO
-import org.m0skit0.android.ikeachallenge.data.api.ProductsDto
 
 internal interface ProductRepository {
-    fun getProducts(): IO<ProductsDto>
+    fun getProducts(): IO<List<Product>>
 }

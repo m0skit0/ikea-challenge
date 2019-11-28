7811239fb1b424adcebc1db2729e444c91838be5
Yassine Diouri <ametric@gmail.com>
2019-11-28 16:29:52 +0100

    Added use case unit tests

---
+++ app/src/test/java/org/m0skit0/android/ikeachallenge/usecase/TestGetProductsUseCaseImpl.kt
@@ -0,0 +1,43 @@
+package org.m0skit0.android.ikeachallenge.usecase
+
+import arrow.fx.IO
+import io.kotlintest.TestCase
+import io.kotlintest.assertions.arrow.either.shouldBeLeftOfType
+import io.kotlintest.assertions.arrow.either.shouldBeRight
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import org.m0skit0.android.ikeachallenge.KoinFreeSpec
+import org.m0skit0.android.ikeachallenge.domain.Product
+import org.m0skit0.android.ikeachallenge.domain.ProductRepository
+import java.io.IOException
+
+class TestGetProductsUseCaseImpl : KoinFreeSpec() {
+
+    @MockK
+    private lateinit var mockProductRepository: ProductRepository
+
+    override val module = module {
+        single { mockProductRepository }
+    }
+
+    override fun beforeTest(testCase: TestCase) {
+        MockKAnnotations.init(this)
+        super.beforeTest(testCase)
+    }
+
+    init {
+        "when product repository throws exception use case must return error" {
+            every { mockProductRepository.getProducts() } returns IO { throw IOException() }
+            runBlocking { GetProductsUseCaseImpl()() }.shouldBeLeftOfType<IOException>()
+        }
+
+        "when product repository returns data use case must return data" {
+            every { mockProductRepository.getProducts() } returns IO { emptyList<Product>() }
+            runBlocking { GetProductsUseCaseImpl()() }.shouldBeRight()
+        }
+    }
+}

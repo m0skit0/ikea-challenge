58066c16e7a11adbee02340581b84c846c048012
m0skit0 <ametric@gmail.com>
2019-12-06 22:56:31 +0100

    Added missing internet permission

--- app/src/main/java/org/m0skit0/android/ikeachallenge/log/Logger.kt
+++ app/src/main/java/org/m0skit0/android/ikeachallenge/log/Logger.kt
@@ -1,6 +1,7 @@
 package org.m0skit0.android.ikeachallenge.log
 
 import arrow.core.Option
+import org.koin.core.context.GlobalContext
 
 internal interface Logger {
     fun debug(message: String)
@@ -8,3 +9,5 @@ internal interface Logger {
     fun warning(message: String)
     fun error(message: String, throwable: Option<Throwable> = Option.empty())
 }
+
+internal fun getLogger(): Logger = GlobalContext.get().koin.get()
